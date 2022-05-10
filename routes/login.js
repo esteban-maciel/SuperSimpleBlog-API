3c6d25807a6bd93e83c2059165be5800c99b6765
esteban-maciel <esteban.maciel@icloud.com>
2022-05-09 17:16:44 -0700

    added token authentication

--- routes/login.js
+++ routes/login.js
@@ -1,42 +1,24 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
 const User = require("../models/user.js");
 const app = express();
 
-
-app.post("/login", async (req, res) => {
-
-    User.findOne({username: req.body.username})
-    .then((user) => {
-        if(!user) {
-            res.status(404).send(user);
+app.post("/login", (req, res) => {
+    User.findOne({ username: req.body.username })
+   .then((user) => {
+        const passwordCompare = bcrypt.compare(req.body.password, user.password)
+        if(passwordCompare) {
+            const token = jwt.sign(user.username, process.env.JWT_SECRET);
+            res.status(200).send(token);
+        } else {
+            res.status(401).status(err);
         }
-        bcrypt.compare(req.body.password, user.password)
-        .then((isCorrect) => {
-            if(isCorrect) {
-                const payload = {
-                    id: user._id,
-                    username: user.username
-                }
-                jwt.sign(
-                    payload,
-                    process.env.JWT_SECRET,
-                    {expiresIn: 86400},
-                    (err, token) => {
-                        if(err) {
-                            res.status(500).send(err);
-                        }
 
-                        res.status(200).send();
-                    }
-                )
-            }
-            else {
-                res.status(404).send();
-            }
-        });
-    });
+   }).catch((err) => {
+       res.status(404).send(err);
+   })
 });
 
 module.exports = app;
