e0376434a2f5f7779b04a1cbd63e8aef0099d350
esteban-maciel <esteban.maciel@icloud.com>
2022-05-13 10:50:02 -0700

    updated

--- routes/login.js
+++ routes/login.js
@@ -4,15 +4,15 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user.js");
 const app = express();
 
-app.post("/login", (req, res) => {
+app.post("/login", async(req, res) => {
     User.findOne({ username: req.body.username })
-   .then((user) => {
-        const passwordCompare = bcrypt.compare(req.body.password, user.password)
+   .then( async (user) => {
+        const passwordCompare =  await bcrypt.compare(req.body.password, user.password)
         if(passwordCompare) {
             const token = jwt.sign(user.username, process.env.JWT_SECRET);
             res.status(200).send(token);
         } else {
-            res.status(401).status(err);
+            return res.status(401).status(err);
         }
 
    }).catch((err) => {
